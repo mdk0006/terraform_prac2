98f5747dc3bc58f002b130f3873e2e32aeb9aa3c
dani <dani.com>
2021-05-26 17:24:14 +0500

    main.tf fmt

--- main.tf
+++ main.tf
@@ -16,30 +16,30 @@ data "aws_ami" "ubuntu" {
 resource "aws_instance" "tf_ec2" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t2.micro"
-  subnet_id = module.vpc.public_subnets[0]
+  subnet_id     = module.vpc.public_subnets[0]
   tags = {
     Environment = "test"
   }
   vpc_security_group_ids = [aws_security_group.sg1.id]
 }
 resource "aws_security_group" "sg1" {
-    name = "sg_tf"
-    description = "This sg is for tf"
-    vpc_id = module.vpc.vpc_id
-    # here we are calling the module output
-    ingress {
-        description = "rule for ssh"
-        from_port = 22
-        to_port = 22
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-    egress {
-        from_port = 0
-        to_port = 0
-        protocol = "-1"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
+  name        = "sg_tf"
+  description = "This sg is for tf"
+  vpc_id      = module.vpc.vpc_id
+  # here we are calling the module output
+  ingress {
+    description = "rule for ssh"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
@@ -57,5 +57,5 @@ module "vpc" {
   tags = {
     Terraform   = "true"
     Environment = "dev"
-}
+  }
 }
